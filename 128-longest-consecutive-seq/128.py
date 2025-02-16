f8dd4a09eb9526aa5577696e05912dace1be6817
Adarsh <111adarshtr111@gmail.com>
2025-02-16 17:00:29 +0530

    Add leet 128

---
+++ 128-longest-consecutive-seq/128.py
@@ -0,0 +1,20 @@
+from heapq import heapify
+
+nums = [1,0,1,2]
+def longestConsecutiveSequence(nums):
+    if len(nums) < 1:
+        return 0
+    nums = sorted(set(nums))
+    maxConsecutive = 1
+    currentConsecutive = 1
+    for i in range(1,len(nums)):
+        if nums[i]-nums[i-1] == 1:
+            currentConsecutive += 1
+        else:
+            currentConsecutive = 1
+        if maxConsecutive < currentConsecutive:
+            maxConsecutive = currentConsecutive
+    return maxConsecutive
+
+print(longestConsecutiveSequence(nums))
+
